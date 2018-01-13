2b4b687cdd4599bcfcbd0d1373cb617df417f147
Marcos Costa <costa.marcos.pro@gmail.com>
2018-01-13 12:37:09 -0200

    Add afterSave mail function

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,35 @@
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.afterSave('Mail', function (request) {
+  var mail = request.object;
+  var installationQuery = new Parse.Query(Parse.Installation);
+
+  // select destination devices
+  installationQuery.equalTo('residence', mail.get('destinationResidence'));
+
+  // format mail to send as push notification
+  var notificationData = {
+    type: 'PUSH_NEW_MAIL',
+    mail: {
+      destination: mail.get('destinationResidence'),
+      comment: mail.get('comment'),
+      created_by: mail.get('createdBy'),
+      created_at: mail.get('createdAt')
+    }
+  };
+
+  // send push notification
+  Parse.Push.send(
+    {
+      where: installationQuery,
+      data: notificationData,
+    },
+    {
+      useMasterKey: true,
+      success: function () {
+        console.log('Mail ' + mail.id + ' afterSave notification send');
+      },
+      error: function (error) {
+        console.error('Error ', error);
+      }
+    }
+  );
 });
